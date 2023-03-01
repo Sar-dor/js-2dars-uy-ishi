799d28af13fca5fc07dfb5d88d77595a37167a9f
Sar-dor <sardorernazarov4555@gmail.com>
2023-03-02 01:27:23 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let UserAge = +prompt('Yoshingizni kiriting')
+if (UserAge <= 18) {
+    console.log("O'qishing kerak")
+
+
+} else if (UserAge > 18 && UserAge <= 60) { console.log("ishlashish kerak") } else {
+    console.log("pensiya yoshiga yetding ")
+}
